da8295add957a0ab2eafa34e7dd77b68ceaf12ac
Diya Shrestha <diyashrestha2001@gmail.com>
2024-08-14 11:25:03 +0545

    LCDemoWrapper Apex Class was created and LCDemo4 component was modified

--- force-app/main/default/aura/LCDemo4/LCDemo4Controller.js
+++ force-app/main/default/aura/LCDemo4/LCDemo4Controller.js
@@ -1,9 +1,14 @@
 ({
 	doInit : function(component, event, helper) {
-		//component.set("v.Var4", "Value from LCDemo4 Controller JavaScript file")
+		component.set("v.Var4", "Value from LCDemo4 Controller JavaScript file")
         var data = {'name' : 'Test Name',
                 'email' : 'test@test.com'};
         
         component.set("v.jsObject", data)
+        
+        component.set("v.userData", {
+            'myString1' : 'String Value',
+            'myInteger1' : 2024
+        })
 	}
 })
