24f56e5c58ff09f2120a222a8349a93ea3ac8872
Diya Shrestha <diyashrestha2001@gmail.com>
2024-08-14 10:55:51 +0545

    listDemo, setDemo, mapDemo and jsObject added in LCDemo Aura Component

--- force-app/main/default/aura/LCDemo4/LCDemo4Controller.js
+++ force-app/main/default/aura/LCDemo4/LCDemo4Controller.js
@@ -1,5 +1,9 @@
 ({
 	doInit : function(component, event, helper) {
-		component.set("v.Var4", "Value from LCDemo4 Controller JavaScript file")
+		//component.set("v.Var4", "Value from LCDemo4 Controller JavaScript file")
+        var data = {'name' : 'Test Name',
+                'email' : 'test@test.com'};
+        
+        component.set("v.jsObject", data)
 	}
 })
