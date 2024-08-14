c3c6428201a254ff62c734a1e5485e253dc9ee10
Diya Shrestha <diyashrestha2001@gmail.com>
2024-08-14 14:35:43 +0545

    event handler assignment added on LCDemo5 Aura Compoent

--- force-app/main/default/aura/LCDemo5/LCDemo5Controller.js
+++ force-app/main/default/aura/LCDemo5/LCDemo5Controller.js
@@ -1,5 +1,22 @@
 ({
+    doInit : function(component, event, helper){
+        //component.set("v.Message1", "Button1 Initialized")
+        //component.set("v.Message2", "Button2 Initialized")
+    },
+    
 	handleClick : function(component, event, helper) {
-        component.set("v.Message", "Button Clicked")
-	}
+        //component.set("v.Message1", "Button Clicked")
+        var btn = event.getSource();
+        var msg = btn.get("v.label");
+        if(msg == "Click Me"){
+            component.set("v.Message1", "Button 1 Clicked");
+        }else{
+            component.set("v.Message1", "Button 2 Clicked");
+        }
+	},
+    
+    anotherhandleClick : function(component, event, helper){
+        //component.set("v.Message2", "Another Button Clicked")
+        component.set("v.Message2", event.getSource().get("v.label"));
+    }
 })
